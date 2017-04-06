bad7d05de41743afb6adcd75430a5fda2f6f5cf3
juanmafc <juanmafc25@gmail.com>
2017-04-06 18:28:32 -0300

    Un par de pruebas en el main (no automatizadas)

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        pruebaGrafo1();
+        pruebaGrafo2();
+        pruebaGrafo3();
+
+    }
+
+
+    private static void pruebaGrafo3() {
+        GrafoDirigido grafo = new GrafoDirigido(7);
+
+        grafo.agregarArista(0, 1);
+        grafo.agregarArista(0, 2);
+
+        grafo.agregarArista(1, 0);
+        grafo.agregarArista(1, 2);
+
+        grafo.agregarArista(2, 0);
+        grafo.agregarArista(2, 1);
+        grafo.agregarArista(2, 3);
+        grafo.agregarArista(2, 4);
+        grafo.agregarArista(2, 5);
+
+        grafo.agregarArista(3, 2);
+        grafo.agregarArista(3, 6);
+
+        grafo.agregarArista(4, 2);
+        grafo.agregarArista(4, 5);
+
+        grafo.agregarArista(5, 2);
+        grafo.agregarArista(5, 4);
+
+        grafo.agregarArista(6, 3);
+
+        System.out.println("Vertices (7)" + grafo.getCantidadVertices());
+        System.out.println("Aristas (16)" + grafo.getCantidadAristas());
+        System.out.println("Adyacentes a 2: 0,1,3,4,5 " + grafo.getVerticesAdyacentesA(2));
+    }
+
+    private static void pruebaGrafo2() {
+        GrafoDirigido grafo = new GrafoDirigido(5);
+        grafo.agregarArista(0, 1);
+        grafo.agregarArista(0, 3);
+        grafo.agregarArista(0, 2);
+        grafo.agregarArista(0, 2);
+        System.out.println("Vertices (5)" + grafo.getCantidadVertices());
+        System.out.println("Aristas (4)" + grafo.getCantidadAristas());
+        System.out.println("Adyacentes a 0: 1,3,2,2 " + grafo.getVerticesAdyacentesA(0));
+        System.out.println(grafo.getVerticesAdyacentesA(2));
+        System.out.println(grafo.getVerticesAdyacentesA(4));
+    }
+
+    private static void pruebaGrafo1() {
+        GrafoDirigido grafo = new GrafoDirigido(2);
+        grafo.agregarArista(0, 1);
+        grafo.agregarArista(1, 0);
+        System.out.println("Vertices " + grafo.getCantidadVertices());
+        System.out.println("Aristas " + grafo.getCantidadAristas());
+        System.out.println(grafo.getVerticesAdyacentesA(0));
+        System.out.println(grafo.getVerticesAdyacentesA(1));
+    }
+}
