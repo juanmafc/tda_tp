4130ce3715791e88f0685ce2515f1daef7ec8c5e
juanmafc <juanmafc25@gmail.com>
2017-04-11 23:16:42 -0300

    Pruebas de componentes fuertemente conexas

--- src/Main.java
+++ src/Main.java
@@ -18,11 +18,122 @@ public class Main {
         pruebaDFS3();
 */
 
+/*
         //TODO: que pasa si el grafo no es conexo?
         pruebaPtoArt1();
         pruebaPtoArt2();
         pruebaPtoArt3();
         pruebaPtoArt4();
+
+*/
+
+        pruebaCFC1();
+        pruebaCFC2();
+        pruebaCFC3();
+        pruebaCFC4();
+        pruebaCFC5();
+
+    }
+
+    private static void pruebaCFC5() {
+        GrafoDirigido grafo = new GrafoDirigido(10);
+
+        grafo.agregarArista(0, 1);
+        grafo.agregarArista(0, 3);
+        grafo.agregarArista(1, 2);
+        grafo.agregarArista(1, 4);
+        grafo.agregarArista(2, 0);
+        grafo.agregarArista(2, 6);
+        grafo.agregarArista(3, 2);
+        grafo.agregarArista(4, 6);
+        grafo.agregarArista(4, 5);
+        grafo.agregarArista(5, 6);
+        grafo.agregarArista(5, 7);
+        grafo.agregarArista(5, 8);
+        grafo.agregarArista(5, 9);
+        grafo.agregarArista(6, 4);
+        grafo.agregarArista(7, 9);
+        grafo.agregarArista(8, 9);
+        grafo.agregarArista(9, 8);
+
+
+        Kosaraju kosaraju = new Kosaraju(grafo);
+        System.out.println("Cantidad de CFCs (4) " + kosaraju.getCantidadCFC() );
+        System.out.println(" (0, 1, 2, 3) (4, 5, 6) (7) (8, 9)");
+        kosaraju.printCFCs();
+    }
+
+
+    private static void pruebaCFC4() {
+        GrafoDirigido grafo = new GrafoDirigido(8);
+
+        grafo.agregarArista(0, 4);
+
+        grafo.agregarArista(1, 0);
+        grafo.agregarArista(1, 5);
+
+
+        grafo.agregarArista(2, 1);
+        grafo.agregarArista(2, 5);
+
+        grafo.agregarArista(3, 6);
+        grafo.agregarArista(3, 7);
+
+        grafo.agregarArista(4, 1);
+
+        grafo.agregarArista(5, 4);
+
+        grafo.agregarArista(6, 2);
+        grafo.agregarArista(6, 5);
+
+        grafo.agregarArista(7, 3);
+        grafo.agregarArista(7, 6);
+
+        Kosaraju kosaraju = new Kosaraju(grafo);
+        System.out.println("Cantidad de CFCs (4) " + kosaraju.getCantidadCFC() );
+        System.out.println(" (0, 1, 4, 5) (2) (6) (3, 7)");
+        kosaraju.printCFCs();
+    }
+
+    private static void pruebaCFC3() {
+        GrafoDirigido grafo = new GrafoDirigido(5);
+
+        Kosaraju kosaraju = new Kosaraju(grafo);
+        System.out.println("Cantidad de CFCs (5) " + kosaraju.getCantidadCFC() );
+        kosaraju.printCFCs();
+    }
+
+
+    private static void pruebaCFC2() {
+        GrafoDirigido grafo = new GrafoDirigido(5);
+        grafo.agregarArista(1, 0);
+        grafo.agregarArista(0, 2);
+        grafo.agregarArista(2, 1);
+
+        grafo.agregarArista(0, 3);
+
+        grafo.agregarArista(3, 4);
+
+        Kosaraju kosaraju = new Kosaraju(grafo);
+        System.out.println("Cantidad de CFCs (3) " + kosaraju.getCantidadCFC() );
+        kosaraju.printCFCs();
+    }
+
+    private static void pruebaCFC1() {
+        GrafoDirigido grafo = new GrafoDirigido(4);
+
+        grafo.agregarArista(0,1);
+        grafo.agregarArista(1,0);
+
+        grafo.agregarArista(2,3);
+        grafo.agregarArista(3,2);
+
+        Kosaraju kosaraju = new Kosaraju(grafo);
+        System.out.println("Cantidad de CFCs (2) " + kosaraju.getCantidadCFC() );
+        kosaraju.printCFCs();
+
+
+
     }
 
     private static void pruebaPtoArt4() {
@@ -31,7 +142,7 @@ public class Main {
         grafo.agregarArista(0,1);
         grafo.agregarArista(0,2);
 
-        DFS dfs = new DFS(grafo);
+        Tarjan dfs = new Tarjan(grafo);
         System.out.println("Ptos de articulacion (0) " + dfs.getPuntosDeArticulacion());
     }
 
@@ -48,13 +159,13 @@ public class Main {
         grafo.agregarArista(5,6);
 
 
-        DFS dfs = new DFS(grafo);
+        Tarjan dfs = new Tarjan(grafo);
         System.out.println("Ptos de articulacion (2, 3) " + dfs.getPuntosDeArticulacion());
     }
 
     private static void pruebaPtoArt2() {
         GrafoNoDirigido grafo = new GrafoNoDirigido(1);
-        DFS dfs = new DFS(grafo);
+        Tarjan dfs = new Tarjan(grafo);
         System.out.println("Ptos de articulacion () " + dfs.getPuntosDeArticulacion());
     }
 
@@ -64,7 +175,7 @@ public class Main {
         grafo.agregarArista(0,1);
         grafo.agregarArista(1,2);
 
-        DFS dfs = new DFS(grafo);
+        Tarjan dfs = new Tarjan(grafo);
         System.out.println("Ptos de articulacion (1) " + dfs.getPuntosDeArticulacion());
     }
 
