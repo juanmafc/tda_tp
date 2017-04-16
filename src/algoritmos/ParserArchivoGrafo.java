f4f0f17579416d765b52091c9613619696e554dd
juanmafc <juanmafc25@gmail.com>
2017-04-16 17:39:07 -0300

    ParserArchivoGrafo puede devolver grafos dirigidos
    Archivos y tests agregados

--- src/algoritmos/ParserArchivoGrafo.java
+++ src/algoritmos/ParserArchivoGrafo.java
@@ -38,8 +38,39 @@ public class ParserArchivoGrafo {
             e.printStackTrace();
         }
 
+        return grafo;
+    }
+
+
+    public GrafoDirigido parsearGrafoDirigido(String archivo) {
+
+        GrafoDirigido grafo = null;
+
+        try {
+            Scanner scan = new Scanner(new FileReader(archivo));
+
+            int cantidadVertices = Integer.parseInt(  ( new Scanner(scan.nextLine()) ).next()  );
+            int cantidadAristas = Integer.parseInt(  ( new Scanner(scan.nextLine()) ).next()  );
+
+            grafo = new GrafoDirigido(cantidadVertices);
+
+            while(scan.hasNextLine())
+            {
+                Scanner linea = new Scanner(scan.nextLine());
+                while(linea.hasNext()){
+
+                    int origen = Integer.parseInt( linea.next() );
+                    int destino = Integer.parseInt( linea.next() );
+
+                    grafo.agregarArista(origen, destino);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         return grafo;
     }
+
 
 }
--- src/tests/ParserArchivoGrafoTest.java
+++ src/tests/ParserArchivoGrafoTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import algoritmos.GrafoDirigido;
 import algoritmos.GrafoNoDirigido;
 import algoritmos.ParserArchivoGrafo;
 import junit.framework.TestCase;
@@ -65,7 +66,63 @@ public class ParserArchivoGrafoTest extends TestCase{
 
         LinkedList<Integer> adyacentes6 = grafo.getVerticesAdyacentesA(6);
         assertEquals(0, adyacentes6.size() );
-
     }
+
+    public void testParsearArchivoGrafoDirigido1() {
+        ParserArchivoGrafo parser = new ParserArchivoGrafo();
+
+        GrafoDirigido grafo = parser.parsearGrafoDirigido("src/tests/grafoDirigidoTest1.txt");
+
+        assertEquals(2, grafo.getCantidadVertices());
+        assertEquals(1, grafo.getCantidadAristas());
+
+        LinkedList<Integer> adyacentes0 = grafo.getVerticesAdyacentesA(0);
+        assertEquals(1, adyacentes0.size() );
+        assertTrue( adyacentes0.contains(1) );
+
+        LinkedList<Integer> adyacentes1 = grafo.getVerticesAdyacentesA(1);
+        assertEquals(0, adyacentes1.size() );
+    }
+
+
+
+
+
+    public void testParsearArchivoGrafoDirigido2() {
+
+        ParserArchivoGrafo parser = new ParserArchivoGrafo();
+
+        GrafoDirigido grafo = parser.parsearGrafoDirigido("src/tests/grafoDirigidoTest2.txt");
+
+        assertEquals(7, grafo.getCantidadVertices());
+        assertEquals(4, grafo.getCantidadAristas());
+
+        LinkedList<Integer> adyacentes0 = grafo.getVerticesAdyacentesA(0);
+        assertEquals(3, adyacentes0.size() );
+        assertTrue( adyacentes0.contains(1) );
+        assertTrue( adyacentes0.contains(2) );
+        assertTrue( adyacentes0.contains(3) );
+
+        LinkedList<Integer> adyacentes1 = grafo.getVerticesAdyacentesA(1);
+        assertEquals(0, adyacentes1.size() );
+
+        LinkedList<Integer> adyacentes2 = grafo.getVerticesAdyacentesA(2);
+        assertEquals(0, adyacentes2.size() );
+
+        LinkedList<Integer> adyacentes3 = grafo.getVerticesAdyacentesA(3);
+        assertEquals(0, adyacentes3.size() );
+
+        LinkedList<Integer> adyacentes4 = grafo.getVerticesAdyacentesA(4);
+        assertEquals(1, adyacentes4.size() );
+        assertTrue( adyacentes4.contains(5) );
+
+        LinkedList<Integer> adyacentes5 = grafo.getVerticesAdyacentesA(5);
+        assertEquals(0, adyacentes5.size() );
+
+        LinkedList<Integer> adyacentes6 = grafo.getVerticesAdyacentesA(6);
+        assertEquals(0, adyacentes6.size() );
+    }
+
+
 
 }
